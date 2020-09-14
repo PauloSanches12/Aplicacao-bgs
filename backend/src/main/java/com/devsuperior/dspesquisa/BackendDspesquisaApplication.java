54a7bc4c47751f33dbf3a8e5461453f012be995f
Paulo <paulo_sanches2012@hotmail.com>
2020-09-14 11:26:26 -0300

    Inicialização do projeto

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/BackendDspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDspesquisaApplication.class, args);
+	}
+
+}
