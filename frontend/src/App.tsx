16d3bd3428010291e1ae7609265ee81e0f47edc0
Paulo <paulo_sanches2012@hotmail.com>
2020-09-18 12:07:36 -0300

    Criação da pagina Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './GlobalStyles.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <Home />
     </>
   );
 }
